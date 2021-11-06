db0ce4d36732170c0f3b27764f3c535754ee5ef2
Crystel Montero <crysvane05@estudiantec.cr>
2021-11-06 00:31:09 -0600

    Ventana de comandos

--- src/Vista/PantallaJugador.java
+++ src/Vista/PantallaJugador.java
@@ -2,21 +2,34 @@ package Vista;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Control;
+import javafx.scene.control.Skin;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
-
-import javax.swing.event.ChangeEvent;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class PantallaJugador implements Initializable {
+
+    private Boolean borrando = false;
+    private Boolean mostrandoError = false;
+    private String lineaComando = "";
+    private ArrayList<Text> errores = new ArrayList<>();
+    private ArrayList<Text> inicios = new ArrayList<>();
 
     @FXML
     private Text arma1At1;
@@ -323,14 +336,91 @@ public class PantallaJugador implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        comandos.skinProperty().addListener(new ChangeListener<Skin<?>>() {
+            @Override
+            public void changed(ObservableValue<? extends Skin<?>> observable, Skin<?> oldValue, Skin<?> newValue) {
+                if (newValue != null && newValue.getNode() instanceof Region) {
+                    Region r = (Region) newValue.getNode();
+                    r.setBackground(Background.EMPTY);
+
+                    r.getChildrenUnmodifiable().stream().
+                            filter(n -> n instanceof Region).
+                            map(n -> (Region) n).
+                            forEach(n -> n.setBackground(Background.EMPTY));
+
+                    r.getChildrenUnmodifiable().stream().
+                            filter(n -> n instanceof Control).
+                            map(n -> (Control) n).
+                            forEach(c -> c.skinProperty().addListener(this));
+                }
+            }
+        });
         Text tI = new Text(">>");
-        tI.setFont(new Font(15));
+        tI.setFont(new Font("Eras Demi ITC", 17));
         tI.setFill(Color.GREEN);
+        inicios.add(tI);
         comandosMostrar.getChildren().add(tI);
         comandos.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!borrando) {
+                    Text comando = new Text(newValue.replace(oldValue, ""));
+                    comando.setText(comando.getText().replace("\n", ""));
+                    comando.setFill(Color.GREEN);
+                    comando.setFont(new Font("Eras Demi ITC", 17));
+                    comandosMostrar.getChildren().add(comando);
+                }
+                comandos.setOnKeyPressed(new EventHandler<KeyEvent>() {
+                    @Override
+                    public void handle(KeyEvent event) {
+                        if (event.getCode() == KeyCode.BACK_SPACE){
+                            borrando = true;
+                            if (comandosMostrar.getChildren().size()>0) {
+                                try {
+                                    Node elemento = comandosMostrar.getChildren().get(comandosMostrar.getChildren().size()-1);
+                                    for (Text i: inicios) {
+                                        if (!elemento.equals(i)) {
+                                            comandosMostrar.getChildren().remove(elemento);
+                                        } else {
+                                            comandosMostrar.getChildren().add(i);
+                                        }
+                                    }
+                                    for (Text t: errores){
+                                        if (!elemento.equals(t)){
+                                            comandosMostrar.getChildren().remove(elemento);
+                                        }else{
+                                            comandosMostrar.getChildren().add(t);
+                                        }
+                                    }
+                                }catch (IllegalArgumentException ignore){}
+                            }
+                        }else{
+                            if (event.getCode() == KeyCode.ENTER){
+                                lineaComando = newValue.replace("\n", "");
+                                if (lineaComando.equals("error")) {
+                                    comandosMostrar.getChildren().add(new Text("\n"));
+                                    Text error = new Text("Esto es un mensaje de error");
+                                    error.setFill(Color.RED);
+                                    error.setFont(new Font("Eras Demi ITC", 17));
+                                    errores.add(error);
+                                    comandosMostrar.getChildren().add(error);
+                                    lineaComando = "";
+                                    comandosMostrar.getChildren().add(new Text("\n"));
+                                }
 
+                                Text inicio = new Text(">>");
+                                inicio.setFont(new Font("Eras Demi ITC", 17));
+                                inicio.setFill(Color.GREEN);
+                                inicios.add(inicio);
+                                comandosMostrar.getChildren().add(new Text("\n"));
+                                comandosMostrar.getChildren().add(inicio);
+                                //hacer algo
+                                lineaComando = "";
+                            }
+                            borrando = false;
+                        }
+                    }
+                });
             }
         });
 
